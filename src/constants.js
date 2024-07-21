9e114a90e4d0205091dc09e3d1da6c5125691218
Rk-Dinesh <rdkumar881@gmail.com>
2024-07-21 19:01:45 +0530

    constant.js

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 //BRAND
 export const name = 'PickMyCourse';
-export const company = 'SeenIT';
+export const company = 'SeenIT PTY LTD';
 export const websiteURL = 'http://localhost:3000';
 export const serverURL = 'http://localhost:5000';
-export const logo = 'https://firebasestorage.googleapis.com/v0/b/pickmycourse-ce045.appspot.com/o/PMC_logos.png?alt=media&token=a752747c-e216-48df-ba6e-e8524f3680bf';
+export const logo = 'https://firebasestorage.googleapis.com/v0/b/pick-my-course-ai.appspot.com/o/PMC_logos.png?alt=media&token=e4c3ed9b-c085-4162-ad6b-1e889b36b4bf';
 export const razorpayEnabled = false;
 export const paypalEnabled = false;
 export const stripeEnabled = true;
@@ -30,20 +30,20 @@ export const YearTime = 'year';
 export const review = "The AI Course Generator revolutionized my content creation process, providing accurate and relevant topics effortlessly. It's a time-saving powerhouse that enhances the quality and relevance of my courses. A must-have tool for educators seeking efficiency and impactful online learning experiences.";
 export const from = "Dinesh Kumar";
 export const profession = 'Software Developer';
-export const photoURL = 'https://media.licdn.com/dms/image/D5603AQEDjPydFO9Z2A/profile-displayphoto-shrink_200_200/0/1683617907233?e=1726704000&v=beta&t=VqAmZII4To8buI_Zj6t-Z3JMuv0i2iFyCSdfB1qeEpU';
+export const photoURL = 'https://media.licdn.com/dms/image/D5603AQEDjPydFO9Z2A/profile-displayphoto-shrink_100_100/0/1683617907233?e=1726704000&v=beta&t=uewK1iXPFGiN0pS1XlqLgoM_aQgW7gz7THrHLttayCk';
 
-// //PAYPAL
+//PAYPAL
 export const paypalPlanIdOne = "";
 export const paypalPlanIdTwo = "";
 
-// //RAZORPAY
+//RAZORPAY
 export const razorpayKeyId = "";
 export const razorpayPlanIdOne = "";
 export const razorpayPlanIdTwo = "";
 
 //STRIPE
 export const stripePlanIdOne = "price_1Pd8mq01PbsRdqnLHOa9bhU1";
-export const stripePlanIdTwo = "";
+export const stripePlanIdTwo = "price_1PduXT01PbsRdqnLVEkLsDGb";
 
 //PAYSTACK
 export const paystackPlanIdOne = "";
